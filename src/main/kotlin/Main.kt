0b77678f3daa4182ff6a8f79e817e55b261aa227
Srinivasa Chari <srinivaschari6268@gmail.com>
2024-12-15 15:30:59 +0530

    create demo search class and implement binary and linear search

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,19 @@
 package com.chari
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    val search = search()
+    val arr = intArrayOf(2, 3, 4, 10, 40)
+    val x = 10
+    val result = search.BinariSearch(arr, x)
+    if (result == -1) {
+        println("Element not present in array")
+    } else {
+        println("Element found at index $result")
+    }
+    val result2 = search.LinearSearch(arr, x)
+    if (result2 == -1) {
+        println("Element not present in array")
+    } else {
+        println("Element found at index $result2")
     }
 }
---
+++ src/main/kotlin/search.kt
@@ -0,0 +1,21 @@
+package com.chari
+
+class search {
+    fun BinariSearch(arr: IntArray, x: Int): Int {
+        var l = 0
+        var r = arr.size - 1
+        while (l <= r) {
+            val m = l + (r - l) / 2
+            if (arr[m] == x) return m
+            if (arr[m] < x) l = m + 1
+            else r = m - 1
+        }
+        return -1
+    }
+    fun LinearSearch(arr: IntArray, x: Int): Int {
+        for (i in arr.indices) {
+            if (arr[i] == x) return i
+        }
+        return -1
+    }
+}
