bca245d098221604dd228dc3e671cc9c4dc93fb0
Srinivasa Chari <srinivaschari6268@gmail.com>
2024-12-15 16:36:11 +0530

    update

--- src/main/kotlin/LinklistMain.kt
+++ src/main/kotlin/LinklistMain.kt
@@ -1,16 +1,21 @@
 package com.chari
 
-import com.chari.ds.LinkedListOperations
-import com.chari.ds.Node
+import com.chari.ds.*
 
 class LinklistMain {
     fun main(){
-        val lm = LinkedListOperations()
-        var head: Node? = null
-        var n = readln().toInt()
-        for (i in 0 until n) {
-            head = lm.insertAtEnd(head, readln().toInt())
+        try {
+            val lm = LinkedListOperations()
+            var head: Node? = null
+            print("Enter the number of elements: ")
+            val n = readln().toInt()
+            for (i in 0 until n) {
+                print("Enter the $i element: ")
+                head = lm.insertAtEnd(head, readln().toInt())
+            }
+            lm.display(head)
+        } catch (e: Exception) {
+            println(e.message)
         }
-        lm.display(head)
     }
 }
---
+++ src/main/kotlin/ds/Node.kt
@@ -0,0 +1,5 @@
+package com.chari.ds
+
+data class Node(var data: Int) {
+    var next: Node? = null
+}
