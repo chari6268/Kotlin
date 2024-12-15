b5a6ebbe1b22fefc0558f6d2489c2eb57db9c5de
Srinivasa Chari <srinivaschari6268@gmail.com>
2024-12-15 15:46:46 +0530

    create a array operation like insertion and delete an element from array

---
+++ src/main/kotlin/ArrayOperations.kt
@@ -0,0 +1,61 @@
+package com.chari
+
+class ArrayOperations {
+    fun deleteElement(arr: IntArray, x: Int): IntArray {
+        val n = arr.size
+        if (n == 0) {
+            return arr
+        }
+        val newArr = IntArray(n - 1)
+        var i = 0
+        var j = 0
+        while (i < n) {
+            if (arr[i] == x) {
+                i++
+                continue
+            }
+            newArr[j] = arr[i]
+            i++
+            j++
+        }
+        return newArr
+    }
+    fun insertElement(arr: IntArray, x: Int, pos: Int): IntArray {
+        val n = arr.size
+        val newArr = IntArray(n + 1)
+        var i = 0
+        var j = 0
+        while (i < n + 1) {
+            if (i == pos) {
+                newArr[i] = x
+                i++
+                continue
+            }
+            newArr[i] = arr[j]
+            i++
+            j++
+        }
+        return newArr
+    }
+    fun updateElement(arr: IntArray, x: Int, pos: Int): IntArray {
+        val n = arr.size
+        if (n == 0) {
+            return arr
+        }
+        val newArr = IntArray(n)
+        var i = 0
+        var j = 0
+        while (i < n) {
+            if (i == pos) {
+                newArr[i] = x
+                i++
+                j++
+                continue
+            }
+            newArr[i] = arr[j]
+            i++
+            j++
+        }
+        return newArr
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,6 +11,26 @@ fun main() {
         arr[i] = readln().toInt()
     }
 
+    // Array operations
+    val arrayOperations = ArrayOperations()
+    print("Enter the element to add: ")
+    val temp = readln().toInt()
+    print("Enter the position to add: ")
+    val pos = readln().toInt()
+    val newArr = arrayOperations.insertElement(arr, temp, pos)
+    println("Array after adding element:")
+    for (i in newArr) {
+        print("$i ")
+    }
+    println()
+    print("Enter the element to delete: ")
+    val temp2 = readln().toInt()
+    val newArr2 = arrayOperations.deleteElement(newArr, temp2)
+    println("Array after deleting element:")
+    for (i in newArr2) {
+        print("$i ")
+    }
+    println()
 //    val arr = intArrayOf(2, 3, 4, 10, 40)
     val x = 10
     val result = search.BinariSearch(arr, x)
